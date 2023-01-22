ce6148aeaec52b4c272a24f905937d3f1384a18a
Lib0823 <robot082373@naver.com>
2023-01-22 21:49:48 +0900

    feat: response 실습코드 작성

--- src/main/java/com/example2/response/controller/ApiController.java
+++ src/main/java/com/example2/response/controller/ApiController.java
@@ -1,17 +1,31 @@
 package com.example2.response.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example2.response.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
+    // TEXT
     @GetMapping("/text")
     public String text(@RequestParam String account) {
         return account;
+    }
+
+    // JSON
+    @PostMapping("/json")
+    public User json(@RequestBody User user) {
+        return user;    // 200 OK
+    }
+
+    @PutMapping("/put")
+    public ResponseEntity<User> put(@RequestBody User user) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        //ResponseEntity.ok(user);
     }
 
 }
---
+++ src/main/java/com/example2/response/dto/User.java
@@ -0,0 +1,59 @@
+package com.example2.response.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL) // Json값에 ~만 포함하겠다 (NULL은 포함하지 않겠다)
+public class User {
+    private String Name;
+    private Integer age;
+    //@JsonProperty("phoneNumber")
+    private String phoneNumber;
+    private String address;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Name='" + Name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
